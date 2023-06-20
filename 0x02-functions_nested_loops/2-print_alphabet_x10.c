216db6449b5f52798ba68ac7ad14adbd28b1091c
doublehigh <ibrahimishaq4real2021@gmail.com>
2023-06-20 09:28:30 -0700

    print alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet 10 times.
+ */
+
+void print_alphabet_x10(void)
+{
+	int a;
+	char alph;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (alph = 'a'; alph <= 'z'; alph++)
+		_putchar(alph);
+	_putchar('\n');
+	}
+}
