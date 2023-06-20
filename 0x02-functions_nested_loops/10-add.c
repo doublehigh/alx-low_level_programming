394cdb53c1de07fcb90de284c986fc81a580ba1d
doublehigh <ibrahimishaq4real2021@gmail.com>
2023-06-20 09:39:17 -0700

    add calculator

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - funciton that add two integers together and print he result
+ * @q: value one indicator
+ * @t: value twon indicator
+ * Return: 0 success
+ */
+
+int add(int q, int t)
+{
+	return (q + t);
+}
