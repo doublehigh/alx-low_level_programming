925d3b2c90c3bb86c53621288fef135122f759a6
doublehigh <ibrahimishaq4real2021@gmail.com>
2023-06-22 16:00:09 -0700

    App that Check for Upper Case

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *  _isupper - check the code
+ *  @c: input character
+ * Return: Always 0
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
